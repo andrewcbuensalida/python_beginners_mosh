1dd7a9467475ae2c0f41af4ee1120ef3e21e62bc
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-10-21 13:56:54 -0700

    num to word

---
+++ num_to_words.py
@@ -0,0 +1,14 @@
+nums = input('Phone: ')
+my_dict = {'0': 'zero',
+           '1': 'one',
+           '2': 'two',
+           '3': 'three',
+           '4': 'four',
+           '5': 'five',
+           '6': 'six',
+           '7': 'seven',
+           '8': 'eight',
+           '9': 'nine',
+           }
+for num in nums:
+    print(my_dict[num] + ' ', end='')
